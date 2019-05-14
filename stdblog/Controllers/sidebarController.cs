81aa3d93450fff22b5bd0dfc499b9b85eb586335
pkay-14 <rquaye15@hotmail.co.uk>
2019-05-15 01:29:24 +0300

    Sidebar Achieved

---
+++ stdblog/Controllers/sidebarController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace stdblog.Controllers
+{
+    public class sidebarController : Controller
+    {
+        // GET: sidebar
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Sidebar()
+        {
+            return PartialView("sidebarmain");
+        }
+        public ActionResult Partial()
+        {
+            return PartialView();
+        }
+    }
+}
